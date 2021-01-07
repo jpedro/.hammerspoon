04cd1a94cb8f85ba1ee3416e6e2d186536cb0bf6
Pedro Barbosa <jpedro.barbosa@gmail.com>
2021-01-07 12:21:07 +0100

    Added missing file in previous commit

--- apps.lua
+++ apps.lua
@@ -12,7 +12,7 @@ hs.hotkey.bind(hyper, "o", function()
   hs.application.launchOrFocus("Visual Studio Code")
 end)
 
-hs.hotkey.bind(hyper, "p", function()
+hs.hotkey.bind(hyper, "b", function()
   -- hs.application.launchOrFocus("Sublime Text")
   hs.application.launchOrFocus("Brave Browser")
 end)
