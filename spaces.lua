01e8187d52edfb9638e6b31c0b18b52752f1cfc9
Pedro Barbosa <jpedro.barbosa@gmail.com>
2022-02-22 00:02:27 +0100

    these confounded tests drive me nuts

--- spaces.lua
+++ spaces.lua
@@ -9,6 +9,12 @@ hs.hotkey.bind({"cmd", "shift"}, "7",      function()
     print(text)
 end)
 
+hs.hotkey.bind({"cmd", "shift"}, "9", function()
+    local win = hs.window.focusedWindow()
+    spaces.moveWindowToSpace(win:id(), 1036)
+    alert("Move to space 1036")
+end)
+
 function newSpace()
     local spaceId = spaces.createSpace()
     alert("Created space " .. spaceId)
@@ -39,11 +45,11 @@ end
 
 function showSpaces()
     local win = hs.window.focusedWindow()
-    local screenId = win:screen():spacesUUID()
-    local spaceId = spaces.activeSpace()
+    local screenId  = win:screen():spacesUUID()
+    local currentId = spaces.activeSpace()
     -- print("----------------- SHOW SPACES")
     print("SCREEN: " .. screenId)
-    print("SPACE:  " .. spaceId)
+    print("SPACE:  " .. currentId)
     local found   = false
     local firstId = 0
     local nextId  = 0
@@ -53,30 +59,25 @@ function showSpaces()
             -- print("-----------------")
             print("==> Screen " .. id)
             for key, val in pairs(screen) do
-                -- print("    (" .. key .. ": " .. val .. ")")
                 local mark = "   "
                 if firstId == 0 then
-                    -- firstId = val
-                    firstId = key
+                    firstId = val
                     mark = " F "
-                    -- print("    FIRST: " .. firstId)
                 end
                 if found and nextId == 0 then
                     nextId = val
                     mark = " N "
-                    -- print("    NEXT: " .. nextId)
                 end
-                if val == spaceId then
+                if val == currentId then
                     found = true
                     mark = ">>>"
-                    -- print("    FOUND: " .. val)
                 end
                 print(mark .. " Space " .. key .. ": " .. val)
             end
         else
             print("... Screen " .. screenId)
             for key, val in pairs(screen) do
-                print("... Space " .. key .. ": " .. val)
+                print("    Space " .. key .. ": " .. val)
             end
         end
     end
@@ -88,8 +89,8 @@ function showSpaces()
 
     print("-----------------")
     print("REPORT")
-    print("    current: " .. spaceId)
-    print("    firstId: " .. firstId)
-    print("    nextId:  " .. nextId)
+    print("    currentId: " .. currentId)
+    print("      firstId: " .. firstId)
+    print("       nextId: " .. nextId)
     return nextId
 end
