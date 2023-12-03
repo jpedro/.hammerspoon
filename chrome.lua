55d5ecd2120099ff238698b2a5af98ec3b508dc4
Pedro Barbosa <jpedro.barbosa@gmail.com>
2023-12-03 11:46:13 +0100

    Andy rebase plx?

--- chrome.lua
+++ chrome.lua
@@ -1,23 +1,23 @@
-local function startsWith(text, find)
-  return text:sub(1, #find) == find
-end
+-- local function StartsWith(text, find)
+--   return text:sub(1, #find) == find
+-- end
 
-local function endsWith(text, find)
+local function EndsWith(text, find)
   return find == "" or text:sub(-#find) == find
 end
 
-function chromeProfile(profile)
+function ChromeProfile(profile)
   for i, window in ipairs(hs.window.allWindows()) do
     local title = window:title()
-    found = string.find(title, "Google Chrome", 1, true)
-    value = has and "true" or "false"
+    local found = string.find(title, "Google Chrome", 1, true)
+    local value = found and "true" or "false"
     print("==> Window: '" .. title .. "' isChrome: " .. value)
 
     if found then
-      found = endsWith(title, profile)
-      value = found and "true" or "false"
+      local hasProfile = EndsWith(title, profile)
+      value = hasProfile and "true" or "false"
       print("==> Window: '" .. title .. "' hasProfile: " .. value)
-      if found then
+      if hasProfile then
         window:focus()
         print("==> And we are done")
         return
